a789179bc146a0aa2ac0bfda3499f7bc74252b4a
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-04 14:06:15 +0200

    function sent to customers component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import { Employees } from './components/Employees';
 import { Employee } from './components/Employee';
+import { Customers } from './components/Customers';
 
 const employeesUrl =
 	'https://raw.githubusercontent.com/graphql-compose/graphql-compose-examples/master/examples/northwind/data/json/employees.json';
@@ -9,6 +10,7 @@ const employeesUrl =
 function App() {
 	const [employees, setEmployees] = useState([]);
 	const [employeeOfTheWeek, setEmployeeOfTheWeek] = useState({});
+	const [customerInfo, setCustomerInfo] = useState('');
 
 	useEffect(() => {
 		(async () => {
@@ -25,7 +27,10 @@ function App() {
 
 	return (
 		<div className="App">
-			<h1>Employees</h1>
+			<h1>Info Site</h1>
+			<p>There are {employees.length} employees.</p>
+			<p>{customerInfo}</p>
+			<h2>Employees</h2>
 			{Object.keys(employeeOfTheWeek).length > 0 && (
 				<>
 					<h2>Employee of the week:</h2>
@@ -34,6 +39,8 @@ function App() {
 			)}
 			<h2>Full list:</h2>
 			<Employees employees={employees} />
+
+			<Customers setCustomerInfo={setCustomerInfo} />
 		</div>
 	);
 }
