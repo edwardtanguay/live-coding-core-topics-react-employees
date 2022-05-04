5ef2cc50ac3a0708d5390756baf7147947266dcc
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-04 11:54:26 +0200

    experiment with state variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-import employees from './data/employees.json';
+
+const employeesUrl =
+	'https://raw.githubusercontent.com/graphql-compose/graphql-compose-examples/master/examples/northwind/data/json/employees.json';
 
 function App() {
+	const [employees, setEmployees] = useState([{firstName: 'test', lastName: 'test2'}]);
+
+	console.log(typeof employees);
+	useEffect(() => {
+		(async () => {
+			// const response = await fetch(employeesUrl);
+			// const _employees = await response.json();
+			const _employees = [...employees]; 
+			_employees.push({ firstName: '222', lastName: '22222' });
+			setEmployees(_employees);
+		})();
+	}, []);
+
 	return (
 		<div className="App">
 			<h1>Employees</h1>
-			{employees.map((emp, index) => {
-				return (
-					<div key={index} className="employee">
-						{emp.firstName} {emp.lastName}
-					</div>
-				);
-			})}
+			<div className="employees">
+				{employees.map((emp, index) => {
+					return (
+						<div key={index} className="employee">
+							{`${emp.firstName} ${emp.lastName}`}
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 }
