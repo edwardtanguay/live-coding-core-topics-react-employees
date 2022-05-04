447e49757c76970d8352d386bdd14d9e392ce0fe
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-04 11:19:46 +0200

    employee shown

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import './App.css'
+import './App.css';
+import employees from './data/employees.json';
 
 function App() {
-  return (
-    <div className="App">
-    <h1>Employees</h1>
-    </div>
-  )
+	return (
+		<div className="App">
+			<h1>Employees</h1>
+			{employees.map((emp, index) => {
+				return (
+					<div key={index} className="employee">
+						{emp.firstName} {emp.lastName}
+					</div>
+				);
+			})}
+		</div>
+	);
 }
 
-export default App
+export default App;
